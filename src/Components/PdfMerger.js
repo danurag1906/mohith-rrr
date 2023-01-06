f784ed6bfd77e39d1f22e4f3d938b1f0c3ad329e
Anurag <danurag1906@gmail.com>
2023-01-06 17:16:03 +0530

    added pdf swap file feature and OCR-text

--- src/Components/PdfMerger.js
+++ src/Components/PdfMerger.js
@@ -1,63 +1,87 @@
-import React from 'react'
 import { useState, useEffect } from "react";
 import PDFMerger from "pdf-merger-js/browser";
+import "./PdfMerger.css";
+import {MdPictureAsPdf} from'react-icons/md'
+import {BsFillArrowDownSquareFill,BsFillArrowUpSquareFill} from 'react-icons/bs'
 import {FaArrowCircleRight} from 'react-icons/fa'
-import './PdfMerger.css'
-import EachFile from "./EachFile";
 
 const PdfMerger = () => {
 
-    const [uploadedFiles, setUploadedFiles] = useState([
-        {
-          id:0,
-          file:''
+    const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [mergedPdfUrl, setMergedPdfUrl] = useState('');
+
+
+  const handleUploadFiles = (files) => {
+    const uploaded = [...uploadedFiles];
+    
+    files.forEach((file) => {
+      uploaded.push(file)
+    });
+    setUploadedFiles(uploaded);
+  };
+
+  const handleFileEvent = (e) => {
+    const chosenFiles = Array.prototype.slice.call(e.target.files);
+    handleUploadFiles(chosenFiles);
+  };
+
+  
+  useEffect(() => {
+    const render = async () => {
+      const merger = new PDFMerger();
+
+      for (const file of uploadedFiles) {
+        await merger.add(file);
+      }
+
+      const mergedPdf = await merger.saveAsBlob();
+      const url = URL.createObjectURL(mergedPdf);
+
+      return setMergedPdfUrl(url);
+    };
+
+    render().catch((err) => {
+      throw err;
+    });
+
+     setMergedPdfUrl();
+  }, [uploadedFiles, setMergedPdfUrl]);
+
+
+  const handleUp=(name)=>{
+    const data=[...uploadedFiles]
+    let index;
+    
+    if(name!=data[0].name){
+      for(let i=0;i<data.length;i++){
+        if(data[i].name==name){
+          index=i;
+          break;
         }
-      ]);
-      const [mergedPdfUrl, setMergedPdfUrl] = useState('');
+      }
+      let temp=data[index];
+      data[index]=data[index-1];
+      data[index-1]=temp;
+      setUploadedFiles(data)
+    }
+  }
+
+  const handleDown=(name)=>{
+    const data=[...uploadedFiles]
+    let index;
     
-    
-      const handleUploadFiles = (files) => {
-        const uploaded = [...uploadedFiles];
-        
-        files.forEach((file) => {
-          let lastIndex=uploaded[uploaded.length-1].id;
-          uploaded.push({
-            id:lastIndex+1,
-            file:file
-          });
-        });
-        setUploadedFiles(uploaded);
-      };
-    
-      const handleFileEvent = (e) => {
-        const chosenFiles = Array.prototype.slice.call(e.target.files);
-        handleUploadFiles(chosenFiles);
-      };
-    
-      let finalFiles=uploadedFiles.filter(file=>file.id!=0)
-      
-      useEffect(() => {
-        const render = async () => {
-          const merger = new PDFMerger();
-    
-          for (let i=0;i<finalFiles.length;i++) {
-            await merger.add(finalFiles[i].file);
-          }
-    
-          const mergedPdf = await merger.saveAsBlob();
-          const url = URL.createObjectURL(mergedPdf);
-    
-          return setMergedPdfUrl(url);
-        };
-    
-        render().catch((err) => {
-          throw err;
-        });
-    
-         setMergedPdfUrl();
-      }, [uploadedFiles, setMergedPdfUrl]);
-    
-    
+    if(name!=data[data.length-1].name){
+      for(let i=0;i<data.length;i++){
+        if(data[i].name==name){
+          index=i;
+          break;
+        }
+      }
+
+      [data[index],data[index+1]]=[data[index+1],data[index]]
+      setUploadedFiles(data)
+    }
+  }
 
   return (
     <div className="App">
@@ -88,11 +112,13 @@ const PdfMerger = () => {
       {!mergedPdfUrl?(
         <p className="danger" >Loading...</p>
       ):(
-        <>
-        {finalFiles.map((file ) => (
-          <EachFile key={file.id} name={file.file.name} />
+        
+        <div className="uploaded-files-list">
+        {uploadedFiles.map((file) => (
+          <div className="eachFile" > <MdPictureAsPdf/> {file.name} <div className="arrows"><BsFillArrowUpSquareFill className="mx-2" onClick={()=>handleUp(file.name)} /><BsFillArrowDownSquareFill onClick={()=>handleDown(file.name)} /></div> </div>
         ))}
-        </>
+        </div>
+        
       )}
 
     </div>
